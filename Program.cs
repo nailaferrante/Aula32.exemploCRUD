23096dd9bc5adffeb93402b3014f6877a9b9c1ae
Pelossi <marcelo.pelossi@hotmail.com>
2017-12-06 20:39:57 -0200

    Console

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
-namespace ExemploCRUD
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace ExemploCRUD {
+    class Program {
+        static void Main (string[] args) {
+            BancoDados bd = new BancoDados ();
+            Categoria cat = new Categoria ();
+
+            Console.WriteLine("Informe o título da categoria a ser adicionado:");
+            cat.Titulo = Console.ReadLine();
+
+            try{
+            if(bd.Adicionar(cat)){
+                Console.WriteLine("Adicionado com sucesso!");
+            }
+            else{
+                Console.WriteLine("Não foi possível incluir.");
+            }
+            }
+            catch (Exception ex) {
+                throw new Exception ("Erro ao cadastrar. " + ex.Message);
+            }
+           
         }
     }
 }
